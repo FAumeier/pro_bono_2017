7d0a0c15c5a7e09b48770f24a8e10dd1a0d19c80
Florian Aumeier <flo.aumeier@gmail.com>
2017-04-22 15:50:52 +0200

    initial project setup

---
+++ src/main/java/RauAPI/ApplicationConfiguration.java
@@ -0,0 +1,14 @@
+package RauAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by flo on 22.04.17.
+ */
+@SpringBootApplication
+public class ApplicationConfiguration {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationConfiguration.class, args);
+    }
+}
